229f320fd033878c9fee5cbb6430980b46918058
Arka Sarkar <arkaskr24@gmail.com>
2025-05-26 15:45:08 +0530

    going_well

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import Banner from '../Components/Banner'
+import LatestCollection from '../Components/LatestCollection'
+import BestSellers from '../Components/BestSellers'
+import OurPolicy from '../Components/OurPolicy'
+import NewsletterBox from '../Components/NewsletterBox'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <LatestCollection/>
+      <BestSellers/>
+      <OurPolicy/>
+      <NewsletterBox/>
+    </div>
+  )
+}
+
+export default Home
