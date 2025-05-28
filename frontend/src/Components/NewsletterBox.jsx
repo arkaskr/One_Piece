06ddbb37884c2a45f748680524fbfcc16d8a5fed
Arka Sarkar <arkaskr24@gmail.com>
2025-05-28 12:37:37 +0530

    update 2.0

--- frontend/src/Components/NewsletterBox.jsx
+++ frontend/src/Components/NewsletterBox.jsx
@@ -8,7 +8,7 @@ const NewsletterBox = () => {
 
   return (
     <div className='text-center'>
-        <p className='text-2xl font-medium text-gray-800'>Become a VIP & Pirate Member just for $20</p>
+        <p className='text-2xl font-medium text-gray-800'>Become a VIP & Pirate Member just for ₹1199</p>
         <p className='text-gray-400 mt-3'>Join us Today and become a member of our crew!</p>
         <form onSubmit={onSubmitHandler} className='rounded-lg w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
             <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
